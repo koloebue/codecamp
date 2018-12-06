392e5688771d1cd082157430a548afcec5a02d69
koloebue <aman_samudinov@mail.ru>
2018-12-06 01:35:48 -0800

    calculator

---
+++ tipcalculator.js
@@ -0,0 +1,9 @@
+
+
+function calculate () {
+    var bill = document.getElementById('bill').value;
+    var tip = document.getElementById('tip').value;
+    var quantityPeople = document.getElementById('people').value;
+    var result = document.getElementById('result').innerHTML = (tip * bill)/quantityPeople;
+
+}
